6d1bf84963d4269250daa3e89f42a857eb576883
Yao Lu <luyaocns@gmail.com>
2020-06-18 17:03:14 +0800

    relu

--- src/reversediff.jl
+++ src/reversediff.jl
@@ -1,5 +1,11 @@
 LoopVectorization.vmap(f, x::ReverseDiff.TrackedArray) = ReverseDiff.track(vmap, f, x)
 
+drelu(x, Δ) = ifelse(x > 0, Δ, zero(x))
+
+ReverseDiff.@grad function Base.broadcasted(f::typeof(relu), x::ReverseDiff.TrackedArray)
+    relu.(x), Δ -> (nothing, drelu.(x, Δ))
+end
+
 ReverseDiff.@grad function vmap(f::typeof(σ), x)
     y = vmap(σ, ReverseDiff.value(x))
     y, Δ -> (nothing, Δ .* (y .* (1 .- y)))
--- src/tracker.jl
+++ src/tracker.jl
@@ -1,5 +1,11 @@
 LoopVectorization.vmap(f, x::Tracker.TrackedArray) = Tracker.track(vmap, f, x)
 
+drelu(x, Δ) = ifelse(x > 0, Δ, zero(x))
+
+Tracker.@grad function Base.broadcasted(::typeof(relu), x::Tracker.TrackedArray)
+    relu.(x), Δ -> (nothing, drelu.(x, Δ))
+end
+
 Tracker.@grad function vmap(::typeof(σ), x)
     y = vmap(σ, Flux.data(x))
     y, Δ -> (nothing, Δ .* (y .* (1 .- y)))
